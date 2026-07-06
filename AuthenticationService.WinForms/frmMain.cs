70b21168498f261d62168594ff6e2fae5ec95e8f
Ahmed Mahmoud <dev.ahmed.kelany@gmail.com>
2026-07-06 18:23:36 +0300

    Initial Project Structure Setup

---
+++ AuthenticationService.WinForms/frmMain.cs
@@ -0,0 +1,10 @@
+namespace AuthenticationService.WinForms
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
