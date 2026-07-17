e45761a2a8a48660c70e8c26344210e3d036d502
Ahmed Mahmoud <dev.ahmed.kelany@gmail.com>
2026-07-17 09:12:46 +0300

    Set up Authentication Files

---
+++ AuthenticationService.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+    }
+}
