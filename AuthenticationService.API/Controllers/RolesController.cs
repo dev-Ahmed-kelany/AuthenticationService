3c340a33e7944ac937916bd75303c19185bcd63a
Ahmed Mahmoud <dev.ahmed.kelany@gmail.com>
2026-07-17 15:12:56 +0300

    Setup Role Repository Files

---
+++ AuthenticationService.API/Controllers/RolesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.API.Controllers
+{
+    [Route("api/roles")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+
+    }
+}
