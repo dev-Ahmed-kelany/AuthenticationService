760a3d99c9f798ae96ef2ca5ea3591f9f769c738
Ahmed Mahmoud <dev.ahmed.kelany@gmail.com>
2026-07-22 11:44:57 +0300

    Add-Feature -> Main Screen

--- AuthenticationService.WinForms/frmMain.Designer.cs
+++ AuthenticationService.WinForms/frmMain.Designer.cs
@@ -28,25 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            lblTest = new Label();
             btnLogin = new Button();
+            msMain = new MenuStrip();
+            usersToolStripMenuItem = new ToolStripMenuItem();
+            listUsersToolStripMenuItem = new ToolStripMenuItem();
+            addNewUserToolStripMenuItem = new ToolStripMenuItem();
+            userDetailsToolStripMenuItem = new ToolStripMenuItem();
+            permissionsToolStripMenuItem = new ToolStripMenuItem();
+            listPermissionsToolStripMenuItem = new ToolStripMenuItem();
+            addNewPermissionToolStripMenuItem = new ToolStripMenuItem();
+            rolesToolStripMenuItem = new ToolStripMenuItem();
+            listRolesToolStripMenuItem = new ToolStripMenuItem();
+            addNewRoleToolStripMenuItem = new ToolStripMenuItem();
+            roleDetailsToolStripMenuItem = new ToolStripMenuItem();
+            loginHistoryToolStripMenuItem = new ToolStripMenuItem();
+            auditLogToolStripMenuItem = new ToolStripMenuItem();
+            msMain.SuspendLayout();
             SuspendLayout();
-            // 
-            // lblTest
-            // 
-            lblTest.AutoSize = true;
-            lblTest.Font = new Font("Times New Roman", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblTest.Location = new Point(65, 68);
-            lblTest.Name = "lblTest";
-            lblTest.Size = new Size(136, 36);
-            lblTest.TabIndex = 0;
-            lblTest.Text = "AuditLog";
             // 
             // btnLogin
             // 
             btnLogin.AutoSize = true;
             btnLogin.Font = new Font("Times New Roman", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnLogin.Location = new Point(487, 420);
+            btnLogin.Location = new Point(977, 517);
             btnLogin.Name = "btnLogin";
             btnLogin.Size = new Size(150, 46);
             btnLogin.TabIndex = 5;
@@ -54,23 +58,139 @@
             btnLogin.UseVisualStyleBackColor = true;
             btnLogin.Click += btnLogin_Click;
             // 
+            // msMain
+            // 
+            msMain.Font = new Font("Times New Roman", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            msMain.Items.AddRange(new ToolStripItem[] { usersToolStripMenuItem, permissionsToolStripMenuItem, rolesToolStripMenuItem, loginHistoryToolStripMenuItem, auditLogToolStripMenuItem });
+            msMain.Location = new Point(0, 0);
+            msMain.Name = "msMain";
+            msMain.Size = new Size(1139, 44);
+            msMain.TabIndex = 6;
+            msMain.Text = "menuStrip1";
+            // 
+            // usersToolStripMenuItem
+            // 
+            usersToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { listUsersToolStripMenuItem, addNewUserToolStripMenuItem, userDetailsToolStripMenuItem });
+            usersToolStripMenuItem.Name = "usersToolStripMenuItem";
+            usersToolStripMenuItem.Size = new Size(102, 40);
+            usersToolStripMenuItem.Text = "Users";
+            // 
+            // listUsersToolStripMenuItem
+            // 
+            listUsersToolStripMenuItem.Name = "listUsersToolStripMenuItem";
+            listUsersToolStripMenuItem.Size = new Size(288, 40);
+            listUsersToolStripMenuItem.Text = "List Users";
+            listUsersToolStripMenuItem.Click += listUsersToolStripMenuItem_Click;
+            // 
+            // addNewUserToolStripMenuItem
+            // 
+            addNewUserToolStripMenuItem.Name = "addNewUserToolStripMenuItem";
+            addNewUserToolStripMenuItem.Size = new Size(288, 40);
+            addNewUserToolStripMenuItem.Text = "Add New User";
+            addNewUserToolStripMenuItem.Click += addNewUserToolStripMenuItem_Click;
+            // 
+            // userDetailsToolStripMenuItem
+            // 
+            userDetailsToolStripMenuItem.Name = "userDetailsToolStripMenuItem";
+            userDetailsToolStripMenuItem.Size = new Size(288, 40);
+            userDetailsToolStripMenuItem.Text = "User Details";
+            userDetailsToolStripMenuItem.Click += userDetailsToolStripMenuItem_Click;
+            // 
+            // permissionsToolStripMenuItem
+            // 
+            permissionsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { listPermissionsToolStripMenuItem, addNewPermissionToolStripMenuItem });
+            permissionsToolStripMenuItem.Name = "permissionsToolStripMenuItem";
+            permissionsToolStripMenuItem.Size = new Size(190, 40);
+            permissionsToolStripMenuItem.Text = "Permissions";
+            // 
+            // listPermissionsToolStripMenuItem
+            // 
+            listPermissionsToolStripMenuItem.Name = "listPermissionsToolStripMenuItem";
+            listPermissionsToolStripMenuItem.Size = new Size(376, 40);
+            listPermissionsToolStripMenuItem.Text = "List Permissions";
+            listPermissionsToolStripMenuItem.Click += listPermissionsToolStripMenuItem_Click;
+            // 
+            // addNewPermissionToolStripMenuItem
+            // 
+            addNewPermissionToolStripMenuItem.Name = "addNewPermissionToolStripMenuItem";
+            addNewPermissionToolStripMenuItem.Size = new Size(376, 40);
+            addNewPermissionToolStripMenuItem.Text = "Add New Permission";
+            addNewPermissionToolStripMenuItem.Click += addNewPermissionToolStripMenuItem_Click;
+            // 
+            // rolesToolStripMenuItem
+            // 
+            rolesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { listRolesToolStripMenuItem, addNewRoleToolStripMenuItem, roleDetailsToolStripMenuItem });
+            rolesToolStripMenuItem.Name = "rolesToolStripMenuItem";
+            rolesToolStripMenuItem.Size = new Size(101, 40);
+            rolesToolStripMenuItem.Text = "Roles";
+            // 
+            // listRolesToolStripMenuItem
+            // 
+            listRolesToolStripMenuItem.Name = "listRolesToolStripMenuItem";
+            listRolesToolStripMenuItem.Size = new Size(287, 40);
+            listRolesToolStripMenuItem.Text = "List Roles";
+            listRolesToolStripMenuItem.Click += listRolesToolStripMenuItem_Click;
+            // 
+            // addNewRoleToolStripMenuItem
+            // 
+            addNewRoleToolStripMenuItem.Name = "addNewRoleToolStripMenuItem";
+            addNewRoleToolStripMenuItem.Size = new Size(287, 40);
+            addNewRoleToolStripMenuItem.Text = "Add New Role";
+            addNewRoleToolStripMenuItem.Click += addNewRoleToolStripMenuItem_Click;
+            // 
+            // roleDetailsToolStripMenuItem
+            // 
+            roleDetailsToolStripMenuItem.Name = "roleDetailsToolStripMenuItem";
+            roleDetailsToolStripMenuItem.Size = new Size(287, 40);
+            roleDetailsToolStripMenuItem.Text = "Role Details";
+            roleDetailsToolStripMenuItem.Click += roleDetailsToolStripMenuItem_Click;
+            // 
+            // loginHistoryToolStripMenuItem
+            // 
+            loginHistoryToolStripMenuItem.Name = "loginHistoryToolStripMenuItem";
+            loginHistoryToolStripMenuItem.Size = new Size(209, 40);
+            loginHistoryToolStripMenuItem.Text = "LoginHistory";
+            loginHistoryToolStripMenuItem.Click += loginHistoryToolStripMenuItem_Click;
+            // 
+            // auditLogToolStripMenuItem
+            // 
+            auditLogToolStripMenuItem.Name = "auditLogToolStripMenuItem";
+            auditLogToolStripMenuItem.Size = new Size(159, 40);
+            auditLogToolStripMenuItem.Text = "AuditLog";
+            auditLogToolStripMenuItem.Click += auditLogToolStripMenuItem_Click;
+            // 
             // frmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1139, 575);
             Controls.Add(btnLogin);
-            Controls.Add(lblTest);
+            Controls.Add(msMain);
+            MainMenuStrip = msMain;
             Name = "frmMain";
             Text = "Main";
             Load += frmMain_Load;
+            msMain.ResumeLayout(false);
+            msMain.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Label lblTest;
         private Button btnLogin;
+        private MenuStrip msMain;
+        private ToolStripMenuItem usersToolStripMenuItem;
+        private ToolStripMenuItem listUsersToolStripMenuItem;
+        private ToolStripMenuItem addNewUserToolStripMenuItem;
+        private ToolStripMenuItem permissionsToolStripMenuItem;
+        private ToolStripMenuItem rolesToolStripMenuItem;
+        private ToolStripMenuItem loginHistoryToolStripMenuItem;
+        private ToolStripMenuItem auditLogToolStripMenuItem;
+        private ToolStripMenuItem listPermissionsToolStripMenuItem;
+        private ToolStripMenuItem addNewPermissionToolStripMenuItem;
+        private ToolStripMenuItem listRolesToolStripMenuItem;
+        private ToolStripMenuItem addNewRoleToolStripMenuItem;
+        private ToolStripMenuItem userDetailsToolStripMenuItem;
+        private ToolStripMenuItem roleDetailsToolStripMenuItem;
     }
 }
--- AuthenticationService.WinForms/frmMain.cs
+++ AuthenticationService.WinForms/frmMain.cs
@@ -2,12 +2,22 @@ using AuthenticationService.WinForms.Models;
 using AuthenticationService.WinForms.API;
 using AuthenticationService.WinForms.Login;
 using AuthenticationService.WinForms.Global;
+using AuthenticationService.WinForms.Users;
+using AuthenticationService.WinForms.Permissions;
+using AuthenticationService.WinForms.Roles;
+using AuthenticationService.WinForms.LoginHistory;
+using AuthenticationService.WinForms.AuditLogs;
 
 namespace AuthenticationService.WinForms
 {
     public partial class frmMain : Form
     {
         frmLogin _Login;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
 
         public frmMain(frmLogin Login)
         {
@@ -16,15 +26,85 @@ namespace AuthenticationService.WinForms
             _Login = Login;
         }
 
-        private async void frmMain_Load(object sender, EventArgs e)
+        private void frmMain_Load(object sender, EventArgs e)
         {
-            lblTest.Text = $"ID: {clsGlobal.CurrentUser.ID}, Username: {clsGlobal.CurrentUser.Username}, Email: {clsGlobal.CurrentUser.Email}";
+
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
             this.Hide();
             _Login.Show();
+        }
+
+        private void listUsersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmUsersList UsersList = new frmUsersList();
+
+            UsersList.Show();
+        }
+
+        private void addNewUserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmAddEditUser AddUser = new frmAddEditUser();
+
+            AddUser.Show();
+        }
+
+        private void userDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmUserDetails UserDetails = new frmUserDetails();
+
+            UserDetails.Show();
+        }
+
+        private void listPermissionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmPermissionsList PermissionsList = new frmPermissionsList();
+
+            PermissionsList.Show();
+        }
+
+        private void addNewPermissionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmAddEditPermission AddPermission = new frmAddEditPermission();
+
+            AddPermission.Show();
+        }
+
+        private void listRolesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmRolesList RolesList = new frmRolesList();
+
+            RolesList.Show();
+        }
+
+        private void addNewRoleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmAddEditRole AddRole = new frmAddEditRole();
+
+            AddRole.Show();
+        }
+
+        private void roleDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmRoleDetails RoleDetails = new frmRoleDetails();
+
+            RoleDetails.Show();
+        }
+
+        private void loginHistoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmLoginHistoryList LoginHistoryList = new frmLoginHistoryList();
+
+            LoginHistoryList.Show();
+        }
+
+        private void auditLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmAuditLogsList AuditLogsList = new frmAuditLogsList();
+
+            AuditLogsList.Show();
         }
     }
 }
