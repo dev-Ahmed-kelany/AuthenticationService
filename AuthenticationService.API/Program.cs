65b4b3b292310f011603875bbb9254644359d5e3
Ahmed Mahmoud <dev.ahmed.kelany@gmail.com>
2026-07-24 20:52:44 +0300

    Last Updates For Production

--- AuthenticationService.API/Program.cs
+++ AuthenticationService.API/Program.cs
@@ -51,7 +51,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors("AllowAngular");
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
--- AuthenticationService.Angular/src/environments/environment.ts
+++ AuthenticationService.Angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // environments/environment.ts
 
 export const environment = {
-  apiUrl: 'https://localhost:7044/api',
+  apiUrl: 'http://authservice-ak.runasp.net/api',
 };
